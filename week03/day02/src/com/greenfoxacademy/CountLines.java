467988f74317fe3f9b9eedf3d76738db54801f7c
niteeshb <niteesh.baranwal@gmail.com>
2018-08-14 14:35:52 +0200

    count lines done

---
+++ week03/day02/src/com/greenfoxacademy/CountLines.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+public class CountLines {
+  public static void main(String[] args) {
+Path path= Paths.get("my_file.txt");
+    try {
+      List list= Files.readAllLines(path);
+      System.out.println("The number of lines in:  "+ list.size());
+    } catch (IOException e) {
+      System.out.println("some prob");;
+    }
+
+
+  }
+}
--- week03/day02/src/com/greenfoxacademy/ReadFile.java
+++ week03/day02/src/com/greenfoxacademy/ReadFile.java
@@ -1,8 +1,4 @@
 package com.greenfoxacademy;
- // Write a program that opens a file called "my-file.txt", then prints
-    // each of the lines form the file.
-    // If the program is unable to read the file (for example it does not exists),
-    // then it should print an error message like: "Unable to read file: my-file.txt"
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,18 +6,16 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class ReadFile {
-      public static void main(String[] args) {
-        try { // Required by Files.readAllLines(filePath);
-          // Reads the content from `lorem-psum.txt` in the `assets` folder line by line to a String List
-          Path filePath = Paths.get("assets/lorem-ipsum.txt");
-          List<String> lines = Files.readAllLines(filePath);
-          for (String s: lines ) {
-
-
-            System.out.println(s); // Prints the first line of the file
-          }
-          } catch (Exception e) {
-          System.out.println("Uh-oh, could not read the file!");
-        }
+  public static void main(String[] args) {
+    try { // Required by Files.readAllLines(filePath);
+      // Reads the content from `lorem-psum.txt` in the `assets` folder line by line to a String List
+      Path filePath = Paths.get("E:\\softwares\\setup.docx");
+      List<String> lines = Files.readAllLines(filePath);
+      for (String s : lines) {
+        System.out.println(s); // Prints the first line of the file
       }
+    } catch (Exception e) {
+      System.out.println("Uh-oh, could not read the file!");
     }
+  }
+}
