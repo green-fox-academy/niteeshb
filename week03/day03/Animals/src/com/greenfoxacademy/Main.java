76dece1880a86ef5afc5df1133cdd6bc1feb5e93
niteeshb <niteesh.baranwal@gmail.com>
2018-08-15 16:12:08 +0200

    Animals project done

---
+++ week03/day03/Animals/src/com/greenfoxacademy/Main.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+      Animal zebra=new Animal();
+      Animal cow=new Animal();
+
+
+      cow.eat();
+      cow.eat();
+
+      cow.play();
+
+      zebra.eat();
+      cow.drink();
+    }
+}
