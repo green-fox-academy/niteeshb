c761a55446ab3c5df47ec37707b171b085e18600
niteeshb <niteesh.baranwal@gmail.com>
2018-08-16 11:44:55 +0200

    bunny2 done

--- week03/day04/Counter/src/com/greenfoxacademy/Bunny2.java
+++ week03/day04/Counter/src/com/greenfoxacademy/Bunny2.java
@@ -1,8 +1,33 @@
 package com.greenfoxacademy;
 
+import java.util.Scanner;
+
 public class Bunny2 {
   // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
 // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+  static int sum=0;
+  public static void main(String[] args) {
+    // write your code here
+    Scanner userinput=new Scanner(System.in);
+    System.out.println("Enter the number of Bunnies");
+    int number=userinput.nextInt();
+    System.out.println("The sum of ears is : " + counterEar(number));
+  }
+
+  private static int counterEar(int number) {
+
+    if(number==0){
+      return sum;
+
+    } else
+      if (number%2==0){
+      sum+=3;
+      }else sum+=2;
+
+    return (counterEar(number-1));
+  }
 }
+
+
