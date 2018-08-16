6801716a0e2c60c319a207f40cc506d7c7327704
niteeshb <niteesh.baranwal@gmail.com>
2018-08-16 16:07:25 +0200

    fibonacci done

--- week03/day04/Counter/src/com/greenfoxacademy/Fibonacci.java
+++ week03/day04/Counter/src/com/greenfoxacademy/Fibonacci.java
@@ -1,4 +1,24 @@
 package com.greenfoxacademy;
 
+import java.util.Scanner;
+
 public class Fibonacci {
+  public static void main(String[] args) {
+    Scanner userinput=new Scanner(System.in);
+    System.out.println("Enter the number for fibnacci series");
+    int number=userinput.nextInt();
+    System.out.println((fibonacchi(number+1)));
+  }
+
+  private static int fibonacchi(int number) {
+
+    if (number<=0){
+      return 0;
+    }else if(number==2){
+      return 1;
+    }else{
+//      System.out.print(number);
+      return fibonacchi(number-1)+ fibonacchi(number-2);
+    }
+  }
 }
