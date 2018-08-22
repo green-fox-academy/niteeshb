c8a3e44571960d0b65ba3381c9b58b0726ce5d25
niteeshb <niteesh.baranwal@gmail.com>
2018-08-22 14:13:43 +0200

    Cloneable done

--- week04/day03/CloneableProject/src/com/greenfoxacademy/Main.java
+++ week04/day03/CloneableProject/src/com/greenfoxacademy/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+      Student student=new Student("John",20,"Male","BME");
+      Student student2= student.clone();
+
+      System.out.println(student2.name);
+      System.out.println(student.name);
     }
 }
--- week04/day03/CloneableProject/src/com/greenfoxacademy/Student.java
+++ week04/day03/CloneableProject/src/com/greenfoxacademy/Student.java
@@ -1,5 +1,40 @@
 package com.greenfoxacademy;
 
-public class Student implements Cloneable {
-  
+public class Student extends Person implements Cloneable {
+  String previousOrganisazion;
+  int skippedDays;
+
+
+  public void getGoals(){
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void introduce(){
+    System.out.println("Hi, I'm "+ name+ ",a "  +age + " year old " +  gender + " from " +previousOrganisazion + " who skipped "+ skippedDays+ " days from the course.");
+  }
+
+  public void skipDays( int numberOfDays){
+    skippedDays+=numberOfDays;
+  }
+
+  public Student(){
+    super();
+    previousOrganisazion="The School of Life";
+    skippedDays=0;
+  }
+
+
+  public Student clone(){
+    return new Student(this.name,this.age,this.gender,previousOrganisazion);
+  }
+
+  public Student(String name,int age,String gender, String previousOrganisazion){
+    super(name,age,gender);
+    this.previousOrganisazion=previousOrganisazion;
+  }
 }
+
+
+
+
+
