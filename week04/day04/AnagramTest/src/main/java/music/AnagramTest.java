84baf3c25246b5de623dbb6cf9ef45c166c08665
niteeshb <niteesh.baranwal@gmail.com>
2018-08-23 16:24:57 +0200

    Anagram test done

--- week04/day04/AnagramTest/src/main/java/music/AnagramTest.java
+++ week04/day04/AnagramTest/src/main/java/music/AnagramTest.java
@@ -1,10 +1,17 @@
 package main.java.music;
 
+import org.junit.Assert;
+
 import static org.junit.Assert.*;
 
 public class AnagramTest {
 
   @org.junit.Test
   public void check() {
+
+    Anagram check=new Anagram();
+    String s1="check";
+    String s2="kcehc";
+    Assert.assertEquals(true,check.check(s1,s2));
   }
 }
