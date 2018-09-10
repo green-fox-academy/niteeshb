c045ef435b620262c5ddc81bcfb4042fe9b3ad84
niteeshb <niteesh.baranwal@gmail.com>
2018-09-10 13:55:21 +0200

    7th exercise done

--- week06/day01/src/FrequencyOfCharacterExercise7.java
+++ week06/day01/src/FrequencyOfCharacterExercise7.java
@@ -1,2 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FrequencyOfCharacterExercise7 {
+  public static void main(String[] args) {
+//    Write a Stream Expression to find the frequency of characters in a given string!
+
+    String string = "My Name Is NiTeEsh";
+    List<String> charList = Arrays.asList(string.split(""));
+    System.out.println(charList.stream()
+            .filter(s->!(s.equals(" ")))
+        .collect(Collectors.groupingBy((s->s),Collectors.counting())));
+
+  }
 }
