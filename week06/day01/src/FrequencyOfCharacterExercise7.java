2b7d32f716a61ffb8274db07c9ae1b26bbbfa02d
niteeshb <niteesh.baranwal@gmail.com>
2018-09-10 13:17:18 +0200

    6th exercise done

---
+++ week06/day01/src/FrequencyOfCharacterExercise7.java
@@ -0,0 +1,2 @@
+public class FrequencyOfCharacterExercise7 {
+}
--- week06/day01/src/StringStartsWithAExercise6.java
+++ week06/day01/src/StringStartsWithAExercise6.java
@@ -5,6 +5,9 @@ public class StringStartsWithAExercise6 {
   public static void main(String[] args) {
 //    Write a Stream Expression to find the strings which starts with A and ends with I in the following array:
 
-    ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+    ArrayList<String> cities = new ArrayList<>(Arrays.asList("AI","ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+  cities.stream()
+          .filter(s->(s.charAt(0)=='A'&&s.charAt(s.length()-1)=='I'))
+          .forEach(System.out::println);
   }
 }
