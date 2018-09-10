ba338150ebe0de165de18397974b32fce2f8ad92
niteeshb <niteesh.baranwal@gmail.com>
2018-09-10 13:58:40 +0200

    8th exercise done

---
+++ week06/day01/src/FrequencyOfNumbersExercise8.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class FrequencyOfNumbersExercise8 {
+  public static void main(String[] args) {
+//    Write a Stream Expression to find the frequency of numbers in the following array:
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+
+    System.out.println( numbers.stream()
+            .collect(Collectors.groupingBy((s->s),Collectors.counting())));
+  }
+}
